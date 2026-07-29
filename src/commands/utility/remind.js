1d69c7be7cc205a33022a6a1eda4fca96ea76fd4
Yamanist <yaman26@proton.me>
2026-07-29 23:15:11 +0300

    clean up mess

--- src/commands/utility/remind.js
+++ src/commands/utility/remind.js
@@ -85,6 +85,7 @@ class Remind extends Command {
             case "remove":
                 this.#remove(interaction, name, data.user);
                 break;
+            // handle unknown commands by showing the list instead
             default:
                 this.#list(interaction, data.user);
         }
