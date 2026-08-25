2ece68b1c7aaf45af0396a1896e67ffcfd5d1235
Yamanist <yaman26@proton.me>
2026-08-25 22:21:20 +0300

    rename vars for clarity

--- src/commands/experimental/wiki.js
+++ src/commands/experimental/wiki.js
@@ -128,6 +128,7 @@ class Wiki extends Command {
                     .setColor(0x0099ff)
                     .setDescription(chunks[i])
                     .setFooter({ text: interaction.t("experimental/wiki:SOURCE_FOOTER") });
+                // Only put the image on the last chunk so it does not repeat
                 if (i === chunks.length - 1) {
                     followUpEmbed.setImage(image);
                 }
